4468e576e5238fc7ec489e669b7f824fb5dafa13
Aravind Chintalapalli <aravindc26@gmail.com>
2013-02-09 01:40:28 +0530

    added asset installer

--- gen/com/aravindc/bharathi/R.java
+++ gen/com/aravindc/bharathi/R.java
@@ -9,23 +9,68 @@ package com.aravindc.bharathi;
 
 public final class R {
     public static final class attr {
+        /** <p>Must be a boolean value, either "<code>true</code>" or "<code>false</code>".
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+         */
+        public static final int state_loaded=0x7f010000;
+    }
+    public static final class color {
+        public static final int button_checked_c1=0x7f050003;
+        public static final int button_checked_c2=0x7f050004;
+        public static final int button_checked_stroke_color=0x7f050005;
+        public static final int button_default_c1=0x7f05000b;
+        public static final int button_default_c2=0x7f05000c;
+        public static final int button_default_stroke_color=0x7f05000d;
+        public static final int button_enabled_c1=0x7f050008;
+        public static final int button_enabled_c2=0x7f050009;
+        public static final int button_enabled_stroke_color=0x7f05000a;
+        public static final int button_inputmode_checked_border_color=0x7f050015;
+        public static final int button_inputmode_loaded_stroke_color=0x7f050006;
+        public static final int button_inputmode_not_loaded_stroke_color=0x7f050007;
+        public static final int button_inputmode_text_disabled=0x7f050014;
+        public static final int button_inputmode_text_loaded=0x7f050012;
+        public static final int button_inputmode_text_notloaded=0x7f050013;
+        public static final int button_inputmode_text_selected=0x7f050011;
+        public static final int button_pressed_c1=0x7f050000;
+        public static final int button_pressed_c2=0x7f050001;
+        public static final int button_pressed_stroke_color=0x7f050002;
+        public static final int button_text_disabled=0x7f05000f;
+        public static final int button_text_enabled=0x7f05000e;
+        public static final int canvas_bg_color=0x7f050016;
+        public static final int canvas_circle_color=0x7f050018;
+        public static final int canvas_drawing_line_color=0x7f05001c;
+        public static final int canvas_first_point_color=0x7f050019;
+        public static final int canvas_last_point_color=0x7f05001b;
+        public static final int canvas_line_color=0x7f05001a;
+        public static final int canvas_text_color=0x7f050017;
+        public static final int drawinput_gui_background=0x7f050010;
+    }
+    public static final class dimen {
+        public static final int button_margin=0x7f060001;
+        public static final int col_width=0x7f060000;
+        public static final int colwidth=0x7f060002;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int button_space=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int menu_settings=0x7f070000;
+        public static final int menu_settings=0x7f0a0000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
     }
     public static final class menu {
-        public static final int activity_main=0x7f060000;
+        public static final int activity_main=0x7f090000;
     }
     public static final class string {
-        public static final int app_name=0x7f040000;
-        public static final int hello_world=0x7f040001;
-        public static final int menu_settings=0x7f040002;
+        public static final int app_name=0x7f070000;
+        public static final int hello_world=0x7f070001;
+        public static final int menu_settings=0x7f070002;
     }
     public static final class style {
         /** 
@@ -49,10 +94,42 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f050000;
+        public static final int AppBaseTheme=0x7f080000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f050001;
+        public static final int AppTheme=0x7f080001;
     }
+    public static final class xml {
+        public static final int method=0x7f040000;
+    }
+    public static final class styleable {
+        /** Attributes that can be used with a InputModeToggleButton.
+           <p>Includes the following attributes:</p>
+           <table>
+           <colgroup align="left" />
+           <colgroup align="left" />
+           <tr><th>Attribute</th><th>Description</th></tr>
+           <tr><td><code>{@link #InputModeToggleButton_state_loaded com.aravindc.bharathi:state_loaded}</code></td><td></td></tr>
+           </table>
+           @see #InputModeToggleButton_state_loaded
+         */
+        public static final int[] InputModeToggleButton = {
+            0x7f010000
+        };
+        /**
+          <p>This symbol is the offset where the {@link com.aravindc.bharathi.R.attr#state_loaded}
+          attribute's value can be found in the {@link #InputModeToggleButton} array.
+
+
+          <p>Must be a boolean value, either "<code>true</code>" or "<code>false</code>".
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+          @attr name android:state_loaded
+        */
+        public static final int InputModeToggleButton_state_loaded = 0;
+    };
 }
